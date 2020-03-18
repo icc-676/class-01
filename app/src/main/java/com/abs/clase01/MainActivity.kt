c80a4e6f16787f5d7e68d6dbd177e9e9c25e8d5d
Pablo de Cárcer <pdecarcer@abstract.cl>
2020-03-18 01:44:23 -0300

    refactor code

--- app/src/main/java/com/abs/clase01/MainActivity.kt
+++ app/src/main/java/com/abs/clase01/MainActivity.kt
@@ -2,6 +2,8 @@ package com.abs.clase01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
@@ -11,33 +13,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setValues()
         button.setOnClickListener {
-            val numberOne = button.text.toString().toInt()
-            val numberTwo = button2.text.toString().toInt()
-            if (numberOne > numberTwo) {
-                Toast.makeText(this,"Respuesta Correcta",Toast.LENGTH_LONG).show()
-            } else if ( numberOne == numberTwo) {
-                Toast.makeText(this,"Respuesta Correcta, pero ojo son iguales",Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this,"Respuesta Incorrecta",Toast.LENGTH_SHORT).show()
-            }
-            setValues()
+            reviewResult(button,button2)
         }
         button2.setOnClickListener {
-            val numberOne = button2.text.toString().toInt()
-            val numberTwo = button.text.toString().toInt()
-            if (numberOne > numberTwo) {
-                Toast.makeText(this,"Respuesta Correcta",Toast.LENGTH_SHORT).show()
-            } else if ( numberOne == numberTwo) {
-                Toast.makeText(this,"Respuesta Correcta, pero ojo son iguales",Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this,"Respuesta Incorrecta",Toast.LENGTH_SHORT).show()
-            }
-            setValues()
-
+            reviewResult(button2,button)
         }
+    }
+    private fun reviewResult(pressedButton: Button, unpressedButton: Button) {
+        val numberOne = pressedButton.text.toString().toInt()
+        val numberTwo = unpressedButton.text.toString().toInt()
+        if (numberOne >= numberTwo)  showAlert("Respuesta Correcta")  else showAlert("Respuesta Incorrecta")
         setValues()
+    }
+
+    private fun showAlert(message: String) {
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
     }
 
     private fun setValues() {
